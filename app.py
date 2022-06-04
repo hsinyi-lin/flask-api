5d02a3e734b04bc2af63eb8e70f5b969cf02b8bf
Hsin Yi Lin <10656025@ntub.edu.tw>
2022-06-04 17:38:02 +0800

    db init

--- app.py
+++ app.py
@@ -1,4 +1,9 @@
-from flask import Flask, jsonify
+import os
+
+from flask import Flask
+from flask_migrate import Migrate
+
+from utils.models import db
 
 app = Flask(__name__)
 
@@ -6,6 +11,12 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return jsonify({'status': True})
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db.init_app(app)
+migrate = Migrate(app, db)
+
 
 
 if __name__ == "__main__":
---
+++ utils/models.py
@@ -0,0 +1,19 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
+class User(db.Model):
+    __tablename__ = 'user'
+    uid = db.Column(db.String(30), primary_key=True)
+    name = db.Column(db.String(20))
+    password = db.Column(db.String(20))
+    admin = db.Column(db.Boolean)
+
+
+class Todo(db.Model):
+    __tablename__ = 'todo'
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.String(100))
+    complete = db.Column(db.Boolean)
+    user_id = db.Column(db.Integer(), db.ForeignKey("user.uid"))
