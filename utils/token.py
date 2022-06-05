b8e5a3595bac952933c047ea7ce7dae5d33bfc80
Hsin Yi Lin <10656025@ntub.edu.tw>
2022-06-05 22:43:45 +0800

    refactor: add success msg

--- utils/token.py
+++ utils/token.py
@@ -14,13 +14,13 @@ def token_required(f):
             token = request.headers['x-access-token']
 
         if not token:
-            return jsonify({'message': 'Token 遺失'}), 401
+            return jsonify({'success': False, 'message': 'Token 遺失'}), 401
 
         try:
             data = jwt.decode(token, 'secret_test', algorithms=['HS256'])
             current_user = User.query.filter_by(uid=data['uid']).first()
         except:
-            return jsonify({'message': 'Token 失效'}), 401
+            return jsonify({'success': False, 'message': 'Token 失效'}), 401
 
         return f(current_user, *args, **kwargs)
 
